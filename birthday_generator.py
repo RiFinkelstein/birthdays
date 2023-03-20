9e2f98a508e652665a348be3d1c62d2a31292261
Your Name <rivkafinkelstein823@gmail.com>
2023-03-20 23:29:21 +0200

    added option to edit birthdays

--- birthday_generator.py
+++ birthday_generator.py
@@ -6,18 +6,31 @@ with open('birthdays.json', 'r') as f:
 
 while True:
     # Ask the user to enter a name
-    name = input("Enter a name (or 'q' to quit): ")
+    option = input("Enter an option: 's' to search for a birthday, 'a' to add a birthday, 'e' to edit a birthday, or 'q' to quit: ")
 
     # Check if the user wants to quit
-    if name == 'q':
+    # Check if the user wants to quit
+    if option == 'q':
         break
 
+
     # Check if the name is in the dictionary
-    if name in birthdays:
-        print(f"{name}'s birthday is {birthdays[name]}")
-    else:
-        print(f"Sorry, we don't have {name}'s birthday in our database.")
-        # Ask the user to enter the birthday and add it to the dictionary
+    elif option == 's':
+        #ask the user to enter a name
+        name = input("Enter a name: ")
+
+        if name in birthdays:
+            print(f"{name}'s birthday is {birthdays[name]}")
+        else:
+            print(f"Sorry, we don't have {name}'s birthday in our database.")
+            # Ask the user to enter the birthday and add it to the dictionary
+            birthday = input(f"What is {name}'s birthday (MM/DD/YYYY)? ")
+            birthdays[name] = birthday
+            print(f"{name}'s birthday has been added to the database.")
+    
+    elif option == 'a':
+        #ask the user to enter a name
+        name= input("enter a name: ")
         birthday = input(f"What is {name}'s birthday (MM/DD/YYYY)? ")
         birthdays[name] = birthday
         print(f"{name}'s birthday has been added to the database.")
@@ -25,3 +38,28 @@ while True:
         # Update the JSON file with the new dictionary
         with open('birthdays.json', 'w') as f:
             json.dump(birthdays, f)
+    
+    elif option == 'e':
+        name= input("enter a name: ")
+        if name in birthdays:
+            # Ask the user to enter the new birthday and update the dictionary
+            birthday = input(f"What is {name}'s new birthday (MM/DD/YYYY)? ")
+            birthdays[name] = birthday
+            print(f"{name}'s birthday has been updated in the database.")
+
+            # Update the JSON file with the new dictionary
+            with open('birthdays.json', 'w') as f:
+                json.dump(birthdays, f)
+        else:
+            print(f"Sorry, we don't have {name}'s birthday in our database.")
+
+    # Invalid option
+    else:
+        print("Invalid option. Please try again.")
+
+
+
+
+
+
+
